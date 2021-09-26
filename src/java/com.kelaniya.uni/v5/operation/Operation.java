edd1f97242e3353b158bf0abe437d6f79bba028b
29Niro <29niro@gmail.com>
2021-09-26 16:35:58 +0530

    Unit test

--- src/java/com.kelaniya.uni/v5/operation/Operation.java
+++
@@ -1,8 +0,0 @@
-package com.kelaniya.uni.v5.operation;
-
-//Abstract class or Interface class
-public interface Operation {
-
-    Double execute(Double[] numbers);
-
-}
---
+++ src/main/java/com/kelaniya/uni/v5/operation/Operation.java
@@ -0,0 +1,8 @@
+package com.kelaniya.uni.v5.operation;
+
+//Abstract class or Interface class
+public interface Operation {
+
+    Double execute(Double[] numbers) throws InvalidCalcOperationException;
+
+}
