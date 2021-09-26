6aad649bbed296759df2f59f7855533748fdc259
29Niro <29niro@gmail.com>
2021-09-26 21:59:03 +0530

    Unit test for OperationFactory

--- src/test/java/com/kelaniya/uni/v5/operation/DivOperationTest.java
+++ src/test/java/com/kelaniya/uni/v5/operation/DivOperationTest.java
@@ -3,7 +3,7 @@ package com.kelaniya.uni.v5.operation;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class DivOperationTest {
 
@@ -20,8 +20,9 @@ public class DivOperationTest {
     public void should_not_divide_by_zero() throws InvalidCalcOperationException {
 
         DivOperation divOperation = new DivOperation();
-        Double result = divOperation.execute(new Double[]{6.0, 0.0});
-        //assertThat(result, is(2.0));
+        assertThat(InvalidOperationException.class, () -> {
+            divOperation.execute(new Double[]{6.0, 0.0});
+        });
     }
 
 }
